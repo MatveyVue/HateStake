68ad0218c23497a89d865d16291c49d995fc2ca5
Whsxg <shabanov.matveyka@bk.ru>
2026-05-11 20:03:48 +0300

    Update api.backup_before_error_message_fix.js

--- src/lib/api.backup_before_error_message_fix.js
+++ src/lib/api.backup_before_error_message_fix.js
@@ -1,4 +1,4 @@
-const API_BASE = 'https://stakingbackend-production.up.railway.app'
+const API_BASE = 'https://api.stakingbackend.railway.app'
 
 let token = localStorage.getItem('scmd_token') || ''
 
